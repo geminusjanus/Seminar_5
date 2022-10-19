e9015fae459f0aea9db888e6308fe8aa9a0d7147
Nikita <silvernikchuprov@gmail.com>
2022-10-19 18:08:44 +0300

    Task 34/2

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -32,32 +32,3 @@ for (int i = 0; i < arr.Length; i++)
         if (arr[i] % 2 == 0)
             count++;
 Console.WriteLine($"Количество четных чисел в массиве равно  {count}");
-
-
-// Console.WriteLine("Введите размер массива");
-// int size = Convert.ToInt32(Console.ReadLine());
-// int[] numbers = new int[size];
-// FillArrayRandomNumbers(numbers);
-// Console.WriteLine("Вот наш массив: ");
-// PrintArray(numbers);
-// int count = 0;
-
-// for (int z = 0; z < numbers.Length; z++)
-// if (numbers[z] % 2 == 0)
-// count++;
-// void FillArrayRandomNumbers(int[] numbers)
-// {
-//     for(int i = 0; i < numbers.Length; i++)
-//     {
-//         numbers[i] = new Random().Next(100,1000);
-//     }
-// }
-// void PrintArray(int[] numbers)
-// {
-//     for(int i = 0; i < numbers.Length; i++)
-//     {
-//         Console.Write(numbers[i] + " ");
-//     }
-//     Console.WriteLine();
-// }
-// Console.WriteLine($"Всего четных чисел из массива: {count}");
