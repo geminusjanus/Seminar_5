89f05fb74b657d67b1b7b2be9ede9da7692aa078
Nikita <silvernikchuprov@gmail.com>
2022-10-19 18:07:48 +0300

    Task34 done

--- Task34/Program.cs
+++ Task34/Program.cs
@@ -2,31 +2,62 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
-
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
-int count = 0;
-
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
-void FillArrayRandomNumbers(int[] numbers)
+int[] CreateArrayRndInt(int size)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    int[] array = new int[size];
+    var rnd = new Random();
+
+    for (int i = 0; i < size; i++)
     {
-        numbers[i] = new Random().Next(100,1000);
+        array[i] = rnd.Next(100, 999);
+
     }
+    return array;
 }
-void PrintArray(int[] numbers)
+void PrintArray(int[] array)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    Console.Write("Массив = [");
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
     }
-    Console.WriteLine();
+    Console.WriteLine("]");
 }
-Console.WriteLine($"Всего четных чисел из массива: {count}");
+int[] arr = CreateArrayRndInt(4);
+PrintArray(arr);
+
+int count = default;
+for (int i = 0; i < arr.Length; i++)
+        if (arr[i] % 2 == 0)
+            count++;
+Console.WriteLine($"Количество четных чисел в массиве равно  {count}");
+
+
+// Console.WriteLine("Введите размер массива");
+// int size = Convert.ToInt32(Console.ReadLine());
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// Console.WriteLine("Вот наш массив: ");
+// PrintArray(numbers);
+// int count = 0;
+
+// for (int z = 0; z < numbers.Length; z++)
+// if (numbers[z] % 2 == 0)
+// count++;
+// void FillArrayRandomNumbers(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         numbers[i] = new Random().Next(100,1000);
+//     }
+// }
+// void PrintArray(int[] numbers)
+// {
+//     for(int i = 0; i < numbers.Length; i++)
+//     {
+//         Console.Write(numbers[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
+// Console.WriteLine($"Всего четных чисел из массива: {count}");
--- Task38/Program.cs
+++ Task38/Program.cs
@@ -2,18 +2,70 @@
 // [3.5, 7.1, 22.9, 2.3, 78.5] -> 76.2
 
 
-Random rnd = new Random();
-int[] d = new int[30];
-int max = 0, min = 100;
-for (int i = 0; i < d.Length; i++) d[i] = rnd.Next(100);
-for (int i = 0; i < d.Length; i++)
+double[] CreateArrayRndDouble (int size)
 {
-    if (d[i] > max) max = d[i];
-    if (d[i] < min) min = d[i];
+    double[] array = new double[size];
+    var rnd = new Random ();
+
+    for (int i = 0; i < size; i++)
+    {
+        double rndNum = rnd.NextDouble() * 100;
+        array[i] = Math.Round(rndNum, 1);
+
+    }
+    return array;
 }
 
-for (int i = 0; i < d.Length; i++) Console.Write($"{d[i]} ");
-Console.WriteLine();
-Console.WriteLine($"max: {max}");
-Console.WriteLine($"min: {min}");
-Console.WriteLine($"Разница максимального и минимального: {max - min}");
+void PrintArray (double[] array)
+{
+    Console.Write("Массив = [");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(i < array.Length -  1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+double min = default;
+double max = default;
+double getSubstraction = default;
+if (array[i] > max) max = d[i];
+if (array[i] < min) min = d[i];
+getSubstraction = max - min;
+Console.WriteLine($"Разница между макс. и мин. элементами массива = {getSubstraction}");
+
+
+double[] arr = CreateArrayRndDouble(5);
+PrintArray(arr);
+
+// double[] GetSubtractionMaxMin (double[] array)
+// {
+//     double getSubstraction = default;
+//     double max = default;
+//     double min = default;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] > max) max = array[i];
+//         if (array[i] < min) min = array[i];
+//     }
+//     getSubstraction = max - min;
+//     return getSubstraction;
+// }
+// Console.WriteLine($"Разница максимального и минимального: {getSubstraction}");
+
+// Random rnd = new Random();
+// int[] d = new int[30];
+// int max = 0, min = 100;
+// for (int i = 0; i < d.Length; i++) d[i] = rnd.Next(100);
+// for (int i = 0; i < d.Length; i++)
+// {
+//     if (d[i] > max) max = d[i];
+//     if (d[i] < min) min = d[i];
+// }
+
+// for (int i = 0; i < d.Length; i++) Console.Write($"{d[i]} ");
+// Console.WriteLine();
+// Console.WriteLine($"max: {max}");
+// Console.WriteLine($"min: {min}");
+// Console.WriteLine($"Разница максимального и минимального: {max - min}");
